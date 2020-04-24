40dc8c0465b1f07a0e43e97b057e09dcc18cc23b
Beep-Roger <martins.775@outlook.com>
2020-04-24 10:44:31 -0300

    Commit inicial

---
+++ JavaDemo/src/demo/InputDemo.java
@@ -0,0 +1,19 @@
+package demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputDemo {
+
+	public static void main(String[] args)throws IOException  {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		System.out.print(str);
+		
+
+	}
+
+}
